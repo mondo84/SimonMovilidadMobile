80c14eea2856a53884a02bf358cfb2d09fbe5ccc
Yedasi <elmondoles@gmail.com>
2026-04-09 14:27:05 -0500

    ref added in login

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,7 +1,11 @@
 import { useAuthStore } from "../store/auth-store";
 
 export const useAuth = () => {
-  const { user, token, isAuthenticated, setAuth, logout } = useAuthStore();
+  const user = useAuthStore((s) => s.user);
+  const token = useAuthStore((s) => s.token);
+  const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
+  const setAuth = useAuthStore((s) => s.setAuth);
+  const logout = useAuthStore((s) => s.logout);
 
   return {
     user,
